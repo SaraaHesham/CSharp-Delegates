58211940176db3d7df970621438d2015fd80c83f
Sara Elghazaly <UG_31094092@F-ENG.TANTA.EDU.EG>
2025-02-17 12:47:52 +0200

    Add files via upload

---
+++ Student Grading System/GradingSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Grading_System
+{
+    internal class GradingSystem
+    {
+        public void DisplayGradingInfo(List<Student> students,
+            Func<List<int>,double> CalculateAverage,
+            Predicate<double> checkIfPassed,
+            Action<Student,double,bool> displayStudentData) 
+        {
+            foreach (var student in students)
+            {
+                double averageGrades = CalculateAverage(student.Grades);
+                bool isStudentPassed = checkIfPassed(averageGrades);
+                displayStudentData(student, averageGrades, isStudentPassed);
+            }
+        }
+    }
+}
---
+++ Student Grading System/Program.cs
@@ -0,0 +1,80 @@
+﻿
+
+
+namespace Student_Grading_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            while (true)
+            {
+                Console.WriteLine("Welcome To My Grading System");
+            
+                Console.Write("Enter Your Name: ");
+                string studentName = Console.ReadLine();
+
+                List<int> studentGrades = new List<int>();
+
+                Console.Write("Enter The Number Of your Grades : ");
+                int numberOfGrades = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < numberOfGrades; i++)
+                {
+                    Console.Write($"Enter Your Grade For The Subject Number {i+1}: ");
+                    int grade = int.Parse(Console.ReadLine());
+                    studentGrades.Add(grade);
+                }
+
+                Student student = new Student();
+                student.Name = studentName;
+                student.Grades = studentGrades;
+                students.Add(student);
+
+                Console.WriteLine("Student Added Successfully");
+
+                Console.WriteLine("Do You Want To Add Another Student? (Y/N)");
+                string answer = Console.ReadLine();
+                
+                if (answer.ToLower() == "n")
+                {
+                    break;
+                }
+
+            }
+
+            GradingSystem gradingSystem = new GradingSystem();
+            gradingSystem.DisplayGradingInfo(students, CalculateAverage01,checkIfPassed01,displayStudentData01);
+
+        }
+
+        private static void displayStudentData01(Student student, double averageGrade, bool isPassed)
+        {
+            Console.WriteLine($"Student Name: {student.Name} , Average Grade: {averageGrade}");
+            if (isPassed)
+            {
+                Console.WriteLine("Student Passed");
+            }
+            else
+            {
+                Console.WriteLine("Student Failed");
+            }
+        }
+
+        private static bool checkIfPassed01(double avgValue)
+        {
+            if (avgValue >= 50)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static double CalculateAverage01(List<int> grades)
+        {
+            return grades.Sum() / grades.Count;
+        }
+    }
+}
